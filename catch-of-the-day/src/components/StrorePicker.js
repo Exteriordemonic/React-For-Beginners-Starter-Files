0eca482716d57b7ed6e75081539c120ada29f048
Kamil Mirosz <kmirosz6@gmail.com>
2018-11-24 18:02:18 +0100

    e_04

---
+++ catch-of-the-day/src/components/StrorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <p>Im a strore picker </p>
+        )
+    }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import StorePicker from './components/StrorePicker';
+
+const app = document.querySelector('#main');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <StorePicker/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render( <App/>, app);
